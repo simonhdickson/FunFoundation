fbbacbbc54cb7922ed431fe72b840dc2db1869a0
Simon Dickson <simonhdickson@gmail.com>
2014-10-03 15:05:39 +0100

    Intial commit of project scaffold

---
+++ src/FunFoundation/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FunFoundation")>]
+[<assembly: AssemblyProductAttribute("FunFoundation")>]
+[<assembly: AssemblyDescriptionAttribute("An FSharp client for FoundationDb")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
